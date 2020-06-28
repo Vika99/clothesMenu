6fedea508487965fa8111bc9febc74317f777e43
Vika <vikavoronchuk@mail.ru>
2020-06-28 23:52:47 +0300

    realization of reflectionFactory.

--- src/main/java/ClothesProject/MainReflection.java
+++ src/main/java/ClothesProject/MainReflection.java
@@ -1,13 +1,21 @@
 package ClothesProject;
 
 import ClothesProject.NotSimpleMenu.ScannerWrapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 
 public class MainReflection {
 
+    @SneakyThrows
     public static void main(String[]args){
         Factory<Clothes> factory= new ReflectionFactory<>(Clothes.class, new ScannerWrapper());
 
         Clothes clothes = factory.create();
         System.out.println(clothes);
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        String clothesJeans = mapper.writeValueAsString(clothes);
+        System.out.println(clothesJeans);
     }
 }
